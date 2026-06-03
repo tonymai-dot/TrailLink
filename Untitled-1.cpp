6522cb4cda62e9d1e1dc63f2bfcf661cbddb3e1e
Mai <tonu.mai@limayrac.fr>
2026-06-03 17:10:05 +0200

    code affichage

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,15 +1,27 @@
-//main.cpp
-#include <iostream>
-#include <Affichage.h>
-#include <buzzer.h>
-/*Mai Tony 02/06/2026*/
-using namespace std;
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define LARGEUR_ECRAN 128 
+#define HAUTEUR_ECRAN 64
+#define ADRESSE_I2C 0x3C
+Adafruit_SSD1306 ecran(LARGEUR_ECRAN, HAUTEUR_ECRAN, &Wire, -1);
+void setup() {
+  Serial.begin(115200);
+  if(!ecran.begin(SSD1306_SWITCHCAPVCC, ADRESSE_I2C)) {
+    Serial.println(F("Erreur : Impossible de trouver l'écran OLED SSD1306"));
+    while(1); // Bloque le programme si l'écran n'est pas détecté
+  }
 
-void main()
-{
+  ecran.clearDisplay();
+  ecran.setTextSize(2);          // Taille des caractères (1 = petit, 2 = moyen, etc.)
+  ecran.setTextColor(SSD1306_WHITE); // Couleur du texte (Blanc sur fond noir)
+  
+  ecran.setCursor(10, 20);       
 
+  ecran.print("bonjour");
 
+  ecran.display();
+}
 
-
-
+void loop() {
 }
